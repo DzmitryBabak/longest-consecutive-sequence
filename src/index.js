3033767070c5b6561b3e976c391dcd6deb1ec24e
Dzmitry Babak <dzmitry.babak@gmail.com>
2018-09-30 22:17:35 +0300

    lcs/implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    // your solution here
+
+    let maxLength = 0;
+    if(array.length === 0){
+        return maxLength;
+    }
+
+    const hashMapArr = {};
+    array.forEach(function(el){
+        hashMapArr[el] = el;
+    });
+
+    let count;
+    let keyNum;
+    for (let i in hashMapArr) {
+        count = 1;
+        keyNum = Number(i);
+        count += findSequence(hashMapArr, keyNum - 1, v => v - 1);
+        count += findSequence(hashMapArr, keyNum + 1, v => v + 1);
+        maxLength = maxLength < count
+            ? count
+            : maxLength
+    }
+    return maxLength;
+};
+
+function findSequence(hashMap, key, nextValue){
+    if(hashMap[key]){
+        delete hashMap[key];
+        return 1 + findSequence(hashMap, nextValue(key), nextValue);
+    }
+    return 0;
+
 }
